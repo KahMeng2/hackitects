2a7d6951f9512b18e801c5cd006c459b0cba78e1
Amy <amy.gh.ly@gmail.com>
2024-04-04 19:20:29 +1100

    router still

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+// Layouts
+import Main from "./layouts/Main";
+
 function mainLoader() {
-  const userName = fetchData("userName");
-  return { userName }
+  return { }
 }
 
 const router = createBrowserRouter([
@@ -17,7 +19,40 @@ const router = createBrowserRouter([
     loader: mainLoader,
     errorElement: <Error />,
     children: [
-
+      {
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        action: dashboardAction
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+        loader: loginLoader
+      },
+      {
+        path: "/profile",
+        element: <ProfilePage />,
+        loader: profileLoader
+      },
+      {
+        path: "/generate",
+        element: <GeneratePage />,
+        loader: generateLoader
+      },
+      {
+        path: "/sucess",
+        element: <Success />,
+        loader: successLoader
+      },
+      {
+        path: ":recipe",
+        element: <RecipePage />,
+      },
+      {
+        path: "/default",
+        element: <DefaultPage />
+      }
     ]
   }
 ]);
