9316b70396fb80f4ca9e000aaa678caea25455c8
Amy <amy.gh.ly@gmail.com>
2024-04-05 02:13:41 +1100

    nav bar again

---
+++ client/src/component/GenButton.jsx
@@ -0,0 +1,14 @@
+import { Form, NavLink, Navigate, useNavigate, Link } from "react-router-dom"
+
+const GenButton = () => {
+
+
+
+    return (
+        <NavLink>
+            <button>
+                GENERATE
+            </button>
+        </NavLink>
+    )
+}
--- client/src/component/NavBar.jsx
+++ client/src/component/NavBar.jsx
@@ -4,13 +4,13 @@ import { mdiAccountCircle } from '@mdi/js';
 
 const Navbar = () => {
     return (
-        <div className="container" style={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
-            <NavLink to="/">
-                <Icon path={mdiAccountCircle} size={2.5} color={"lightcoral"} className="usericon"/>
-            </NavLink>
-            <span style={{color:"lightcoral", fontFamily: "Lilita One", fontSize: 80, paddingLeft: 150}}>
+        <div className="container" style={{display: "flex", alignItems: "center", justifyContent: "space-between", backgroundColor: "#BEC9C0", width: "100vw"}}>
+            <span style={{color:"#688D84", fontFamily: "Lilita One", fontSize: 80}}>
                 underCOOK'D
             </span>
+            <NavLink to="/">
+                <Icon path={mdiAccountCircle} size={2.5} color={"#688D84"} className="usericon"/>
+            </NavLink>
         </div>
     )
 }
