629aa081ba478905bb529fad81b6f20dbd0fc6a0
KahMeng2 <kahmeng.lee12@gmail.com>
2024-04-04 14:46:13 +1100

    added front end base code

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
