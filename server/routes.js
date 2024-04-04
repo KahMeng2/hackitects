22ab415cd87df11bf441e48b8559efc5e2763b6f
Laradell Tria <ltria@student.unimelb.edu.au>
2024-04-05 00:50:00 +1100

    feat: Ingredients Model

--- server/routes.js
+++ server/routes.js
@@ -3,10 +3,12 @@ import express from "express";
 // Imports route api
 import sampleApi from "./controller/sample.js";
 import recipeApi from "./controller/recipeController.js";
+import ingredientApi from "./controller/ingredientController.js";
 
 const api = express.Router();
 
 api.use("/sample", sampleApi);
 api.use("/recipe", recipeApi);
+api.use("/ingredients", ingredientApi);
 
 export default api;
