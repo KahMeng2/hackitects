bd1921eeda72cfb2eb6c627e31bbf1cca76fbb1d
Amy <amy.gh.ly@gmail.com>
2024-04-04 16:44:11 +1100

    added react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,15 @@
 import viteLogo from '/vite.svg'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([]);
+
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return ( <div></div>
+    
   )
 }
 
