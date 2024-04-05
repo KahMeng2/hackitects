d640dbbdb3b2081a5fa51babc6e3cb4f302cc7d6
Amy <amy.gh.ly@gmail.com>
2024-04-05 23:04:03 +1100

    landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,9 +1,23 @@
 import Banner from "../component/Banner";
+import Scroll from "../component/Scroll";
+
+const recipes = [
+    { title: 'Spaghetti Carbonara', image: '/src/assets/spaghetti-carbonara.jpg' },
+    { title: 'Chicken Curry', image: '/src/assets/chicken-curry.jpg' },
+    { title: 'Chocolate Cake', image: '/src/assets/chocolate-cake.jpg' },
+    { title: 'Spaghetti Carbonara', image: '/src/assets/spaghetti-carbonara.jpg' },
+    { title: 'Chicken Curry', image: '/src/assets/chicken-curry.jpg' },
+    { title: 'Chocolate Cake', image: '/src/assets/chocolate-cake.jpg' }
+    // Add more recipes as needed
+];
+
 
 const LandingPage = () => {
     return (
         <div>
             <Banner/>
+            <Scroll recipes={recipes}/>
+
         </div>
     )
 }
