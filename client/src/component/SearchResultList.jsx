43c0f21130e17ead171c6042e7dd13c7e282cdbe
Yuki Leung <yukileung523@gmail.com>
2024-04-05 17:03:33 +1100

    search bar draft

---
+++ client/src/component/SearchResultList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./SearchResultList.css";
+
+export const SearchResultList = () => {
+    return (
+        <div className="results-list">
+            <div>A</div>
+            <div>B</div>
+            <div>C</div>
+        </div>
+    );
+};
