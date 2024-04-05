66c80044b4615f2e35ae6a2045981019d155959a
Amy <amy.gh.ly@gmail.com>
2024-04-05 16:43:35 +1100

    separated and tested components

--- client/layouts/main.jsx
+++ client/layouts/main.jsx
@@ -3,24 +3,13 @@ import img from "../src/assets/banner.webp"
 
 // Components
 import Navbar from "../src/component/NavBar"
-import GenButton from "../src/component/GenButton"
+import Banner from "../src/component/Banner"
 
 const Main = () => {
     console.log("something")
         return (
             <div>
                 <Navbar/>
-                <div className="container" style={{position: "relative", display:"flex", justifyContent:"center"}}>
-                    <img src={img} style={{objectFit:"cover", width:"100%", height:"400px", filter:"sepia(30%) brightness(80%) contrast(110%)"}}></img>
-                    <p style={{position: "absolute", top:"15%", bottom:"50%", color:"white", fontFamily:"Gaegu", fontSize:"70px"}}>
-                        Let's get cooking with
-                        <div style={{textAlign:"center", fontFamily:"Lilita One"}}>underCOOK'D</div>
-                    </p>
-                </div>
-                
-                <div>
-                    <GenButton/>
-                </div>
                 <main>
                     <Outlet/>
                 </main>
