69e0e5bea3f5fbd43f165a018b1238f21e9010b9
Amy <amy.gh.ly@gmail.com>
2024-04-06 04:55:45 +1100

    sign up box styled

--- client/src/component/UserAndPass.jsx
+++ client/src/component/UserAndPass.jsx
@@ -1,18 +1,32 @@
 import { Form } from "react-router-dom"
 
+
 const UserAndPW = () => {
+
+    const styles = {
+        input: {
+            color:"#84A59D",
+            backgroundColor:"#ECE5DE", 
+            border: "none",
+            borderRadius: "5px",
+            fontWeight: "500",
+            paddingLeft:"10px",
+            paddingRight:"70px",
+            paddingTop:"10px",
+            paddingBottom:"10px"
+        },
+    }
 
     return (
         <div>
-            <div className="input-group mb-3">
-                <label>
-                    Username: <input defaultValue="Type your username"/>
-                </label>
+            <h1 style={{color:"#688D84", fontFamily:"Readex Pro", margin:"50px", fontWeight:"700"}}>Sign Up</h1>
+            <div className="input-group mb-3" style={{fontFamily:"Readex Pro", display:"block"}}>
+                <h4 style={{color:"#688D84", fontWeight:"500", textAlign:"left"}}>Username</h4>
+                <input placeholder="Type your username" style={styles.input}/>
             </div>
-            <div className="input-group mb-3">
-                <label>
-                    Password: <input type="password"/>
-                </label>
+            <div className="input-group mb-3" style={{fontFamily:"Readex Pro", display:"block"}}>
+                <h4 style={{color:"#688D84", fontWeight:"500", textAlign:"left"}}>Password</h4>
+                <input type="password" style={styles.input}/>
             </div>
         </div>
     )
--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -2,7 +2,7 @@ import UserAndPW from "../component/UserAndPass"
 
 const SignUpPage = () => {
     return (
-        <div style={{backgroundColor:"#BEC9C0"}}>
+        <div style={{backgroundColor:"#BEC9C0", display:"flex", alignSelf:"center", justifyContent:"center"}}>
 
             <UserAndPW/>
         </div>
